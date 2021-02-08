b92afdf4ddc9ddf63f863a7eb6ac7db8b8ad1287
Sathya Hari <sathyahari9@gmail.com>
2021-02-08 03:20:34 -0500

    Added relationships to child model

--- test/models/child_test.rb
+++ test/models/child_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class ChildTest < ActiveSupport::TestCase
   # TODO: Relationship macros
-  
+  should validate_presence_of(:first_name)
+  should validate_presence_of(:last_name)
   # TODO: Validation macros
 
   # TODO: Context testing
